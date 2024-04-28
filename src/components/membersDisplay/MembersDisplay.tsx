974a9013139c4b16708566e4c6f5f342920bfef9
Adnan Sait <adnansait74@gmail.com>
2024-04-28 17:02:47 +0530

    Adds no content msg on admin table

--- src/components/membersDisplay/MembersDisplay.tsx
+++ src/components/membersDisplay/MembersDisplay.tsx
@@ -1,11 +1,33 @@
-import { User } from "../../page/admin/AdminPage";
-
+import useAdminTableContext from "../../hooks/context/useAdminTableContext";
+import useQueryParams from "../../hooks/useQueryParams";
 import AdminTable from "../adminTable/AdminTable";
+
+import classes from "./MembersDisplay.module.css";
+import { User } from "../../page/admin/AdminPage";
 
 type MembersDisplayProps = {
   membersList: User[] | undefined;
 };
 
 export default function MembersDisplay({ membersList }: MembersDisplayProps) {
-  return <AdminTable membersList={membersList} />;
+  const [params] = useQueryParams();
+  const { totalLength } = useAdminTableContext();
+
+  const searchTerm = params.get("search");
+
+  let noMembersMsg;
+  if (totalLength > 0 && membersList?.length === 0 && searchTerm) {
+    noMembersMsg = `Could not find any users with the search term - ${searchTerm}`;
+  } else if (totalLength === 0) {
+    noMembersMsg = `No users found`;
+  }
+
+  return (
+    <div>
+      <AdminTable membersList={membersList} />
+      {noMembersMsg && (
+        <div className={classes["no-content-msg"]}>{noMembersMsg}</div>
+      )}
+    </div>
+  );
 }
