b1e0cc80149e78032ed145c63d8a96dbe5399056
Adnan Sait <adnansait74@gmail.com>
2024-04-28 16:18:11 +0530

    Integrates search with query params

--- src/components/adminSearch/AdminSearch.tsx
+++ src/components/adminSearch/AdminSearch.tsx
@@ -1,3 +1,5 @@
+import { ChangeEvent, FormEvent, useRef } from "react";
+import useQueryParams from "../../hooks/useQueryParams";
 import Button from "../../ui/Button/Button";
 import CloseMark from "../../ui/icons/CloseMark";
 import MagnifyingGlass from "../../ui/icons/MagnifyingGlass";
@@ -5,24 +7,66 @@ import MagnifyingGlass from "../../ui/icons/MagnifyingGlass";
 import classes from "./AdminSearch.module.css";
 
 export default function AdminSearch() {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const [params, setParams] = useQueryParams();
+  const searchInput = params.get("search") ?? "";
+
+  function handleFormSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const val = inputRef.current?.value ?? "";
+
+    updateSearchParam(val);
+  }
+
+  function handleSearchInput(event: ChangeEvent<HTMLInputElement>) {
+    const val = event.target.value.trim();
+    updateSearchParam(val);
+  }
+
+  function updateSearchParam(searchText: string) {
+    setParams((state) => {
+      if (searchText) {
+        state.set("search", searchText);
+      } else {
+        state.delete("search");
+      }
+      return state;
+    });
+  }
+
+  function handleSearchClear() {
+    inputRef.current?.focus();
+    setParams((state) => {
+      state.delete("search");
+      return state;
+    });
+  }
+
   return (
-    <form className={classes["search-form"]}>
+    <form onSubmit={handleFormSubmit} className={classes["search-form"]}>
       <input
+        ref={inputRef}
         className={classes["search-input"]}
         type="text"
         name="search"
         placeholder="Search by name, email or role"
+        value={searchInput}
+        onChange={handleSearchInput}
       />
 
       <section className={classes["search-icon-group"]}>
-        <Button
-          variant="icon"
-          type="reset"
-          className={classes["clear-icon"]}
-          aria-label="Clear search"
-        >
-          <CloseMark />
-        </Button>
+        {searchInput.length > 0 && (
+          <Button
+            variant="icon"
+            type="button"
+            className={classes["clear-icon"]}
+            aria-label="Clear search"
+            onClick={handleSearchClear}
+          >
+            <CloseMark />
+          </Button>
+        )}
         <Button
           variant="icon"
           type="submit"
