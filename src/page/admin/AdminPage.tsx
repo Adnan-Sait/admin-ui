3a2e4c9c9124bc33c27b59d86657e53a035d07ca
Adnan Sait <adnansait74@gmail.com>
2024-04-28 16:38:59 +0530

    Implemented search functionality

--- src/page/admin/AdminPage.tsx
+++ src/page/admin/AdminPage.tsx
@@ -1,12 +1,14 @@
+import { useEffect, useMemo } from "react";
+
 import AdminHeader from "../../components/adminHeader/AdminHeader";
 import MembersDisplay from "../../components/membersDisplay/MembersDisplay";
 import { getMembers } from "../../services/userService";
 import useQuery from "../../hooks/useQuery";
+import useAdminTableContext from "../../hooks/context/useAdminTableContext";
+import useQueryParams from "../../hooks/useQueryParams";
 import Pagination from "../../components/pagination/Pagination";
 
 import classes from "./AdminPage.module.css";
-import useAdminTableContext from "../../hooks/context/useAdminTableContext";
-import { useEffect } from "react";
 
 export type User = {
   id: string;
@@ -19,8 +21,22 @@ export default function AdminPage() {
   const itemsPerPage = 10;
   const { data, error, isLoading } = useQuery<User[] | null>(getMembers);
 
-  const { members, setMembers, activePage } = useAdminTableContext();
+  const { members, setMembers, activePage, setActivePage } =
+    useAdminTableContext();
+  const [params] = useQueryParams();
+
+  const searchTerm = params.get("search") ?? "";
+  const searchDataset = useMemo(prepareSearchDataset, [members]);
+
+  const filteredMembers = useMemo(filterMembers, [
+    members,
+    searchDataset,
+    searchTerm,
+  ]);
+
   const [startCount, endCount, paginatedData] = getPaginatedData();
+
+  const filteredListLength = filteredMembers?.length ?? 0;
 
   useEffect(() => {
     if (!error && data) {
@@ -29,17 +45,48 @@ export default function AdminPage() {
   }, [data, error, setMembers]);
 
   function getPaginatedData(): [number, number, User[]] | [] {
-    if (!members || !activePage) return [];
+    if (!filteredMembers || !activePage) return [];
 
     const startIndex = (activePage - 1) * itemsPerPage;
-    const endIndex = Math.min(activePage * itemsPerPage, members.length);
+    const endIndex = Math.min(
+      activePage * itemsPerPage,
+      filteredMembers.length
+    );
 
     const slicedData = [];
     for (let index = startIndex; index < endIndex; index++) {
-      slicedData.push(members[index]);
+      slicedData.push(filteredMembers[index]);
     }
 
     return [startIndex + 1, endIndex, slicedData];
+  }
+
+  function prepareSearchDataset(): Map<string, string> {
+    const searchData = new Map<string, string>();
+
+    if (!members) return searchData;
+
+    members.forEach((item) => {
+      searchData.set(item.id, `${item.name},${item.email},${item.role}`);
+    });
+
+    return searchData;
+  }
+
+  function filterMembers(): User[] | null {
+    if (searchDataset.size === 0 || !members || !searchTerm) {
+      return members;
+    }
+
+    const memberIds: string[] = [];
+
+    searchDataset.forEach((value, key) => {
+      if (value.toLowerCase().includes(searchTerm.toLowerCase())) {
+        memberIds.push(key);
+      }
+    });
+
+    return members.filter((item) => memberIds.includes(item.id));
   }
 
   if (isLoading) {
@@ -55,7 +102,14 @@ export default function AdminPage() {
       <AdminHeader startItem={startCount ?? 0} endItem={endCount ?? 0} />
       <MembersDisplay membersList={paginatedData} />
       <aside className={classes["pagination-container"]}>
-        <Pagination itemsPerPage={itemsPerPage} />
+        {filteredListLength > 0 && (
+          <Pagination
+            totalCount={filteredListLength}
+            itemsPerPage={itemsPerPage}
+            activePage={activePage}
+            setActivePage={setActivePage}
+          />
+        )}
       </aside>
     </div>
   );
