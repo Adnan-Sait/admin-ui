9bff9f85bec2293c0eeec80a96768df06abb2f1d
Adnan Sait <adnansait74@gmail.com>
2024-05-01 23:13:30 +0530

    Adds toast and progress components

---
+++ src/components/toast/Toast.tsx
@@ -0,0 +1,43 @@
+import classNames from "classnames";
+import Button from "../../ui/Button/Button";
+
+import classes from "./Toast.module.css";
+import useAppContext from "../../hooks/context/useAppContext";
+import Progress from "../../ui/Progress/Progress";
+
+export default function Toast() {
+  const {
+    toast: { toastTitle, toastVariant, showToast },
+    toastDispatch,
+  } = useAppContext();
+
+  function handleLoadingComplete() {
+    toastDispatch({ type: "hideToast" });
+  }
+
+  function handleToastClose() {
+    toastDispatch({ type: "hideToast" });
+  }
+
+  if (!toastTitle || !showToast) return null;
+
+  return (
+    <dialog
+      className={classNames(classes.toast, classes[`toast--${toastVariant}`])}
+      open
+    >
+      <Progress
+        className={classes.progress}
+        max={3000}
+        type="decrement"
+        onComplete={handleLoadingComplete}
+      />
+      <div className={classes["toast-body"]}>
+        <h3>{toastTitle}</h3>
+        <Button variant="icon" onClick={handleToastClose}>
+          Close
+        </Button>
+      </div>
+    </dialog>
+  );
+}
