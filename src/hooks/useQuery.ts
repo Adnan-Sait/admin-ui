76323f106a088a2531d0afa545552394f7396aa4
Adnan Sait <adnansait74@gmail.com>
2024-05-02 10:56:01 +0530

    Adds refetch function to usequery

--- src/hooks/useQuery.ts
+++ src/hooks/useQuery.ts
@@ -1,20 +1,32 @@
 import { useEffect, useState } from "react";
 import ApiResponse from "../models/ApiResponse";
 
-export default function useQuery<T>(callback: () => Promise<ApiResponse<T>>) {
+/**
+ *
+ * @param apiCall Callback should be a stable reference
+ */
+export default function useQuery<T>(apiCall: () => Promise<ApiResponse<T>>) {
   const [data, setData] = useState<T | null>(null);
   const [error, setError] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
+  const [refetchCount, setRefetchCount] = useState(0);
 
   useEffect(() => {
     (async () => {
-      const apiResponse = await callback();
+      const apiResponse = await apiCall();
 
       setData(apiResponse.data);
       setError(apiResponse.error);
       setIsLoading(false);
     })();
-  }, [callback]);
+  }, [apiCall, refetchCount]);
 
-  return { data, error, isLoading };
+  /**
+   * Causes the API call to run again.
+   */
+  function refetch() {
+    setRefetchCount((state) => state + 1);
+  }
+
+  return { data, error, isLoading, refetch };
 }
