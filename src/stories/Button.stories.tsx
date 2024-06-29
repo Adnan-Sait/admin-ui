bec402eea16706a45687a7592fcdf5bf571fbae4
Adnan Sait <adnansait74@gmail.com>
2024-06-29 19:23:26 +0530

    Adds storybook for Stories

---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Button from "../ui/Button/Button";
+
+const meta: Meta<typeof Button> = {
+  title: "Button",
+  component: Button,
+  argTypes: { type: { type: "string" } },
+};
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const Primary: Story = {
+  args: {
+    variant: "primary",
+    children: "Hello World",
+  },
+};
